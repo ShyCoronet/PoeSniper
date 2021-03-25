6f2b945af040125871bb30ceae7ae14ed8d84dc5
ShyCoronet <mr.chegulov@gmail.com>
2021-03-25 19:26:42 +0300

    add new commands

--- PoeSniper/ApplicationViewModel.cs
+++ PoeSniper/ApplicationViewModel.cs
@@ -1,9 +1,8 @@
-﻿using PoeSniperCore;
+﻿using CefSharp;
+using PoeSniper;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.ComponentModel;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -12,6 +11,7 @@ namespace PoeSniperUI
     public class ApplicationViewModel
     {
         public IList<LiveSearch> searches { get; }
+        public IList<string> searchResults { get; }
 
         private const int _maxSearchCount = 12;    // the maximum number of searches for an official trade is 12
 
@@ -32,14 +32,43 @@ namespace PoeSniperUI
             }
         }
 
+        private RelayCommand _removeSearch;
+        public RelayCommand RemoveSearch
+        {
+            get
+            {
+                return _removeSearch ?? (_removeSearch = new RelayCommand<LiveSearch>((search) => 
+                {
+                    searches.Remove(search);
+                    search.TradeObserver.Dispose();
+                }));
+            }
+        }
+
+        private RelayCommand _removeAllSearches;
+        public RelayCommand RemoveAllSearches
+        {
+            get
+            {
+                return _removeAllSearches ?? (_removeAllSearches = new RelayCommand(() =>
+                {
+                    foreach (var search in searches)
+                    {
+                        search.TradeObserver.Dispose();
+                    }
+                    searches.Clear();
+                }));
+            }
+        }
+
         private RelayCommand _connect;
         public RelayCommand Connect
         {
             get
             {
-                return _connect ?? (_connect = new RelayCommand<LiveSearch>(async (search) =>
+                return _connect ?? (_connect = new RelayCommand<LiveSearch>((search) =>
                 {
-                    await Task.Run(() => search.TradeObserver.LoadRessource());
+                    Task.Run(() => search.TradeObserver.LoadRessource());
                 },
                 (search) =>
                 {
@@ -48,20 +77,44 @@ namespace PoeSniperUI
             }
         }
 
-        private RelayCommand _startSearch;
-        public RelayCommand StartSearch
+        private RelayCommand _connectAllSearches;
+        public RelayCommand ConnectAllSearches
         {
             get
             {
-                return _startSearch ?? (_startSearch = new RelayCommand<LiveSearch>(async (search) =>
+                return _connectAllSearches ?? (_connectAllSearches = new RelayCommand(() =>
+                {
+                    foreach (var search in searches)
+                    {
+                        if (!search.IsLoading)
+                            Task.Run(() => search.TradeObserver.LoadRessource());
+                    }
+                }));
+            }
+        }
+
+        private RelayCommand _changeStateSearch;
+        public RelayCommand ChangeStateSearch
+        {
+            get
+            {
+                return _changeStateSearch ?? (_changeStateSearch = new RelayCommand<LiveSearch>(async (search) =>
                 {
                     if (!search.IsActive)
                     {
-                        await Task.Run(() => search.TradeObserver.StartObserve());
+                        await Task.Run(() =>
+                        {
+                            search.TradeObserver.TradeOfferReceived += OnTradeOfferReceived;
+                            search.TradeObserver.StartObserve();
+                        });
                     }
                     else
                     {
-                        await Task.Run(() => search.TradeObserver.StopObserve());
+                        await Task.Run(() =>
+                        {
+                            search.TradeObserver.TradeOfferReceived -= OnTradeOfferReceived;
+                            search.TradeObserver.StopObserve();
+                        });
                     }
                 }));
             }
@@ -69,13 +122,22 @@ namespace PoeSniperUI
 
         public ApplicationViewModel()
         {
-            searches = new ObservableCollection<LiveSearch>() { new LiveSearch() };
-            searches[0].TradeObserver.TradeOfferReceived += TradeObserver_TradeOfferReceived;
+            searches = new ObservableCollection<LiveSearch>();
+            searchResults = new ObservableCollection<string>();
         }
 
-        private void TradeObserver_TradeOfferReceived(object sender, CefSharp.ConsoleMessageEventArgs e)
+        private void OnTradeOfferReceived(object sender, CefSharp.ConsoleMessageEventArgs e)
         {
-            
+            string tradeOfferMessage = Clipboard.GetText();
+            object lockObj = new();
+            lock (lockObj)
+            {
+
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    searchResults.Add(tradeOfferMessage);
+                });
+            }
         }
     }
 }
