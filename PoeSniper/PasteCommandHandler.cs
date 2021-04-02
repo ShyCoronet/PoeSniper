b89d87f4a2efc031303152fca75c73434065e2e3
ShyCoronet <mr.chegulov@gmail.com>
2021-04-02 19:10:55 +0300

    implementation pastecommandhandler

---
+++ PoeSniper/PasteCommandHandler.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KeyboardHook;
+
+namespace PoeSniperUI
+{
+    public class PasteCommandHandler
+    {
+        private GlobalKeyboardHook keyHook;
+        private Tuple<Key, Key> hotKey = new(Key.LCONTROL, Key.KEY_V);
+        private Key lastKey;
+
+        public event EventHandler Pasted;
+
+        public PasteCommandHandler()
+        {
+            keyHook = new GlobalKeyboardHook();
+            keyHook.KeyDown += OnPaste;
+        }
+
+        public void Install()
+        {
+            keyHook.Install();
+        }
+
+        private void OnPaste(Key key)
+        {
+            if (key == hotKey.Item1)
+            {
+                lastKey = key;
+            }
+
+            if (lastKey == hotKey.Item1 && key == hotKey.Item2)
+            {
+                lastKey = default;
+                Pasted?.Invoke(null, null);
+            }
+        }
+
+
+        ~PasteCommandHandler()
+        {
+            keyHook.Uninstall();
+        }
+    }
+}
