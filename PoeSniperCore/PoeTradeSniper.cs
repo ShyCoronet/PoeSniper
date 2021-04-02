51e0556bd7db788d23167ee67a206f9f5ae12e2f
ShyCoronet <mr.chegulov@gmail.com>
2021-04-02 19:07:05 +0300

    add new property, refactor, change script

--- PoeSniperCore/PoeTradeSniper.cs
+++ PoeSniperCore/PoeTradeSniper.cs
@@ -9,13 +9,42 @@ namespace PoeSniperCore
     {
         private OffscreenBrowser browser;
         private bool isInitialScript;
+        private bool isActive;
+        private bool isLoading;
+        private bool isConnected;
 
         public string Url { get; set; }
-        public bool IsActive { get; private set; }
-        public bool IsLoading { get; private set; }
+        public bool IsActive
+        {
+            get => isActive;
+            private set
+            {
+                isActive = value;
+                SniperStateChanged?.Invoke(this, new SniperStateChangedEventArgs(value));
+            }
+        }
+        public bool IsLoading
+        {
+            get => isLoading;
+            private set
+            {
+                isLoading = value;
+                LoadingStateChanged?.Invoke(this, new LoadingStateChangedEventArgs(value));
+            }
+        }
+        public bool IsConnected
+        {
+            get => isConnected;
+            private set
+            {
+                isConnected = value;
+                ConnectionStateChanged?.Invoke(this, new ConnectionStateChangedEventArgs(value));
+            }
+        }
 
-        public event EventHandler<ObserverStateChangedEventArgs> ObserverStateChanged;
+        public event EventHandler<SniperStateChangedEventArgs> SniperStateChanged;
         public event EventHandler<LoadingStateChangedEventArgs> LoadingStateChanged;
+        public event EventHandler<ConnectionStateChangedEventArgs> ConnectionStateChanged; 
         public event EventHandler<ConsoleMessageEventArgs> TradeOfferReceived
         {
             add => browser.ConsoleMessageReceive += value;
@@ -33,19 +62,19 @@ namespace PoeSniperCore
             browser.SetCookie("https://www.pathofexile.com/", "POESESSID", sessionId);
         }
 
-        public void LoadRessource()
+        public void LoadPage()
         {
             if (IsActive) StopSnipe();
 
+            IsConnected = false;
+            isInitialScript = false;
             IsLoading = true;
-            LoadingStateChanged?.Invoke(this, new LoadingStateChangedEventArgs(IsLoading));
-
+            
             browser.LoadPage(Url).Wait();
-
             Task.Delay(1000).Wait(); // Required for full execution of all scripts on the page
 
             IsLoading = false;
-            LoadingStateChanged?.Invoke(this, new LoadingStateChangedEventArgs(IsLoading));
+            IsConnected = CheckPoeTradePage();
         }
 
         public bool StartSnipe()
@@ -56,7 +85,6 @@ namespace PoeSniperCore
 
             var result = browser.ExecuteJavaScriptAsync(script).Result;
             IsActive = result.Success;
-            ObserverStateChanged?.Invoke(this, new ObserverStateChangedEventArgs(result.Success));
 
             return IsActive;
         }
@@ -68,11 +96,7 @@ namespace PoeSniperCore
             if (IsActive)
             {
                 var result = browser.ExecuteJavaScriptAsync(script).Result;
-                if (result.Success) 
-                {
-                    IsActive = false;
-                    ObserverStateChanged?.Invoke(this, new ObserverStateChangedEventArgs(false));
-                };
+                IsActive = !result.Success;
             }         
         }
 
@@ -84,11 +108,22 @@ namespace PoeSniperCore
         private void InitialScript()
         {
             string script = "const config = { childList: true }\n" +
-                                "const callback = (mutationList, observer) => { const whisperBtn = document.querySelector('button.btn.btn-default.whisper-btn')\nif(whisperBtn !== null) { console.log(whisperBtn._v_clipboard.text()) } }\n" +
-                                "const target = document.querySelector('div.results')\n" +
-                                "const observer = new MutationObserver(callback)\n";
+                            "const callback = (mutationList, observer) => { const whisperBtn = document.querySelector('button.btn.btn-default.whisper-btn')\nif(whisperBtn !== null) { console.log(whisperBtn._v_clipboard.text()) } }\n" +
+                            "const target = document.querySelector('div.results')\n" +
+                            "const observer = new MutationObserver(callback)\n";
 
             isInitialScript = browser.ExecuteJavaScriptAsync(script).Result.Success;
+        }
+
+        private bool CheckPoeTradePage()
+        {
+            string script = "const checkingScript = () => {let result = document.querySelector('button.btn.livesearch-btn')" +
+                                                            ".children[1].textContent === 'Deactivate Live Search'\n" +
+                                                            "if(!result) throw 'Not live search' }\n" +
+                                                            "checkingScript()";
+            bool isPoeTradeLiveSearch = browser.ExecuteJavaScriptAsync(script).Result.Success;  
+
+            return isPoeTradeLiveSearch;
         }
     }
 }
