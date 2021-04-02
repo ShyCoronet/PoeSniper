474d9a11dab4039f630e8116e8a19cf13626a31b
ShyCoronet <mr.chegulov@gmail.com>
2021-04-02 19:10:32 +0300

    implementation notification service

---
+++ PoeSniper/NotificationService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Data.Xml.Dom;
+using Windows.UI.Notifications;
+
+namespace PoeSniperUI
+{
+    public class NotificationService
+    {
+        public void ShowNotification(string message)
+        {
+            string body = $@"<toast>
+                      <visual>
+                        <binding template='ToastGeneric'>
+                          <text>TradeOffer</text>
+                          <text>{message}</text>
+                        </binding>
+                      </visual>
+                    </toast>";
+
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(body);
+
+            ToastNotification toast = new ToastNotification(doc);
+            ToastNotificationManager.CreateToastNotifier().Show(toast);
+        }
+    }
+}
