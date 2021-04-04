01e73c14a5640d90f5e8ee2641fedd960ad8873b
ShyCoronet <mr.chegulov@gmail.com>
2021-04-04 13:30:02 +0300

    add new event args

---
+++ PoeSniperCore/EventsArgs/TradeOfferMessageEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PoeSniperCore.EventsArgs
+{
+    public class TradeOfferMessageEventArgs : EventArgs
+    {
+        public string TradeOfferMessage { get; }
+
+        public TradeOfferMessageEventArgs(string message)
+        {
+            TradeOfferMessage = message;
+        }
+    }
+}
