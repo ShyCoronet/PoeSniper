3b5d6b14ff2d69ce5cb3d9e95da3a9a1decf2996
ShyCoronet <mr.chegulov@gmail.com>
2021-04-04 13:22:47 +0300

    cosmetic

--- PoeSniper/MainWindow.xaml.cs
+++ PoeSniper/MainWindow.xaml.cs
@@ -1,26 +1,9 @@
-﻿using ChromuimBrowser;
-using PoeSniperCore;
-using PoeSniperUI;
-using PoeSniperUI.CustomControls;
+﻿using PoeSniperUI;
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Dynamic;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Interop;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace PoeSniper
 {
