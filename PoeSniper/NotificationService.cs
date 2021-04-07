c3a643f7fc1bce6d7fcbd89afb02ac7bb59a67b5
ShyCoronet <mr.chegulov@gmail.com>
2021-04-07 18:18:59 +0300

    change notification builder

--- PoeSniper/NotificationService.cs
+++ PoeSniper/NotificationService.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Windows.Data.Xml.Dom;
-using Windows.UI.Notifications;
+using Microsoft.Toolkit.Uwp.Notifications;
 
 namespace PoeSniperUI
 {
@@ -12,20 +11,7 @@ namespace PoeSniperUI
     {
         public void ShowNotification(string name, string message)
         {
-            string body = $@"<toast>
-                      <visual>
-                        <binding template='ToastGeneric'>
-                          <text>{name}</text>
-                          <text>{message}</text>
-                        </binding>
-                      </visual>
-                    </toast>";
-
-            XmlDocument doc = new XmlDocument();
-            doc.LoadXml(body);
-
-            ToastNotification toast = new ToastNotification(doc);
-            ToastNotificationManager.CreateToastNotifier().Show(toast);
+            new ToastContentBuilder().AddText(name).AddText(message).Show();
         }
     }
 }
