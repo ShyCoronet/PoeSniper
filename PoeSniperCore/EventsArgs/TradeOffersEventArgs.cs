a360e8c8c37236fc79d9e231a862f9a8d9150d98
ShyCoronet <mr.chegulov@gmail.com>
2021-04-21 19:12:52 +0300

    rework

---
+++ PoeSniperCore/EventsArgs/TradeOffersEventArgs.cs
@@ -0,0 +1,16 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+
+namespace PoeSniperCore.EventsArgs
+{
+    public class TradeOffersEventArgs : EventArgs
+    {
+        public IEnumerable<TradeOffer> TradeOffers { get; }
+
+        public TradeOffersEventArgs(IEnumerable<TradeOffer> offers)
+        {
+            TradeOffers = offers;
+        }
+    }
+}
